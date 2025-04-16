0bdd46b4a82f53ffcf1a2110188eba73b63cb831
Derek Lee <dereklee@ps.asl.com.hk>
2025-04-16 19:01:41 +0800

    init (java 17+spring boot 3.4.4)

---
+++ src/main/java/io/github/dereklpm/javaassignment202504/JavaAssignment202504Application.java
@@ -0,0 +1,13 @@
+package io.github.dereklpm.javaassignment202504;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssignment202504Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAssignment202504Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/dereklpm/javaassignment202504/JavaAssignment202504ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dereklpm.javaassignment202504;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssignment202504ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
