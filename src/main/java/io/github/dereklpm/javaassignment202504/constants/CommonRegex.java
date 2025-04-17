ed7bde2013df40b2ece8b83f9e86a1393966ef52
Derek Lee <dereklee@ps.asl.com.hk>
2025-04-17 21:13:08 +0800

    update the numeric validation ignore empty string

--- src/main/java/io/github/dereklpm/javaassignment202504/constants/CommonRegex.java
+++ src/main/java/io/github/dereklpm/javaassignment202504/constants/CommonRegex.java
@@ -1,5 +1,5 @@
 package io.github.dereklpm.javaassignment202504.constants;
 
 public class CommonRegex {
-    public static final String ALPHANUMERIC = "^[a-zA-Z0-9 ]+$";
+    public static final String ALPHANUMERIC = "^$|^[a-zA-Z0-9 ]+$";
 }
